130048f6fe21e4a3dfd7b43eeb47cc64287b7c4b
Valentina Nies <valunies@gmail.com>
2020-11-09 17:20:29 -0300

    prueba config

--- database/config/config.js
+++ database/config/config.js
@@ -1,7 +1,7 @@
 module.exports= {
   "development": {
     "username": "root",
-    "password": "@harru09",
+    "password": "",
     "database": "proyecto_integrador",
     "host": "127.0.0.1",
     "port": 3306,
