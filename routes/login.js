cb78126ed04067f172f425342bf322c06895ca9b
Valentina Nies <valunies@gmail.com>
2020-11-10 19:26:56 -0300

    login

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,9 @@
 let express=require("express");
 let router=express.Router();
 let loginController=require('../controllers/loginController')
+
 router.get("/", loginController.index)
+
+// router.post("/login", userController.login)
+
 module.exports=router;
