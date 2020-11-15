12dd21146b6823f48680c18799076b4015076871
Valentina Nies <valunies@gmail.com>
2020-11-15 19:51:21 -0300

    punto 7 incompleto

--- controllers/postController.js
+++ controllers/postController.js
@@ -5,8 +5,22 @@ const op= db.Sequelize.Op;
 
 let postController={
     detallePost: function(req, res){
+        let id = req.params.id;
+        
+        db.Post.findByPk(id,
+            {include:[
+                {association: "users"},
+                {association: "comentarios",
+                    include: {association: "comments_user"}
+                }
+            ]},)
+    
+        .then(function(post){
+            res.render("detallePost", {post: post})
+        })
         return res.render('detallePost')
     },
+
     agregarPost: function(req, res){
         if(req.session.userLogueado != null){
         return res.render('agregarPost')
@@ -14,21 +28,43 @@ let postController={
             return res.send("Solo los usuarios logueados pueden agregar posteos")
         }
     },
-    storePost: function(req, res){
-        req.body.img
-        req.body.description
 
-        let newPost = {
-            id_usuario: req.session.userLogueado.id,
-            url_img: req.body.img,
-            post_descr: req.body.description,
+    storePost: function(req, res){
+        if (req.session.userLogueado == undefined) {
+            res.redirect("/login")
         }
-        db.Post.create(newPost)
-        .then(function(){
-            res.redirect("/home")
-        },
-        )
+        if (req.session.userLogueado != undefined) {
+                let newPost = {
+                    id_usuario: req.session.userLogueado.id,
+                    nombre_usuario: req.session.userLogueado.nombre_usuario,
+                    url_img: req.body.url_img,
+                    post_descr: req.body.post_descr,
+                    published: req.body.published,
+                };      
+        
+                db.Post.create(newPost)
+        
+                .then(function(){
+                    res.redirect("/home");
+                })
+        } else {
+                res.redirect("/user/registracion")
+            }        
+    },
+    
+    borrarPost: function(req, res){
+        
+        let idBorrarPost = req.params.id;
+
+        db.Post.destroy({
+            where: {
+                id: idBorrarPost
+            }
+        });
+
+        return res.redirect('/home');
     }
 }
+
 
 module.exports=postController;
