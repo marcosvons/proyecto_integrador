9c515591c63463702a78d4e93d37c2afd47c3985
Marcos von Stremayr <mvonstremayr@udesa.edu.ar>
2020-11-15 19:55:09 -0300

    home terminado, mi perfil terminado

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,9 @@ let usersController=require('../controllers/usersController');
 router.get("/detalleUsuario", usersController.detalleUsuario);
 router.get("/miPerfil", usersController.miPerfil);
 router.get("/resultadoBusqueda", usersController.resultadoBusqueda);
+router.get("/edit/:id", usersController.edit)
 
 router.post('/logout', usersController.logout)
+router.post("/edit/:id", usersController.updateUsername)
 
 module.exports = router;
