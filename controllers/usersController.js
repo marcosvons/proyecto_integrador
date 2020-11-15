cc55506f3a63f00a8d2614307121f1f7762627fb
Valentina Nies <valunies@gmail.com>
2020-11-14 23:06:09 -0300

    buscador

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,8 @@
+const db= require('../database/models');
+const bcrypt=require('bcryptjs')
+const users=db.User;
+const op= db.Sequelize.Op;
+
 let usersController={
     detalleUsuario: function(req, res){
         return res.render('detalleUsuario')
@@ -10,7 +15,19 @@ let usersController={
             }
     },
     resultadoBusqueda: function(req, res){
-        return res.render('resultadoBusqueda')
+        let busqueda = req.query.busqueda;
+        db.User.findAll(
+            {
+                where: {
+                    [op.or]: [{ nombre_usuario: { [op.like]: "%" + busqueda + "%"} }, { email: { [op.like]: "%" + busqueda + "%"} }
+                ],
+                order: ["nombre_usuario"]
+                }
+            }
+        )
+        .then(function(usuarioResultado){
+            res.render("resultadoBusqueda", {usuarioResultado: usuarioResultado, busqueda: busqueda})
+        })
     },
     logout: function(req, res){
         req.session.destroy();
--- routes/post.js
+++ routes/post.js
@@ -1,7 +1,11 @@
 let express=require("express");
 let router=express.Router();
 let postController=require('../controllers/postController')
+
+//Para q el usuario complete el form
 router.get("/agregarPost", postController.agregarPost);
+
+//Para leer y procesar lo q el user completo en el form
 router.get("/detallePost", postController.detallePost);
 
 module.exports=router;
